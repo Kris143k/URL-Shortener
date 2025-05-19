299c8a974d202472613720cb69b672ba53d28406
Kris143k <130545320+Kris143k@users.noreply.github.com>
2025-05-20 04:00:33 +0530

    Update auth.js

--- Service/auth.js
+++ Service/auth.js
@@ -2,20 +2,22 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
+const User = require('../models/user');
+
 const jwt = require('jsonwebtoken');
 const secret = process.env.JWT_SECRET || 'defaultsecretkey';
 
 // function setUser(id, user) {
 //   sessionToUserMap.set(id, user);
 // }
-function setUser(user) {
-  const token = jwt.sign({
-    email: user.email,
-    name: user.name,
-    id: user._id,
-    roles: user.roles,
-  }, secret);
-  return token;
+async function getUser(token) {
+  try {
+    const decoded = jwt.verify(token, secret);
+    const user = await User.findById(decoded.id);
+    return user;
+  } catch (err) {
+    return null;
+  }
 }
 
 // function getUser(id) {
